37b2a2b6b62b7514b5275b85bc354245fa1e9918
Ryan Crossan <ryan.crossan@LT40110.local>
2017-12-13 09:21:07 -0800

    update log extension to html

--- statusScanner.js
+++ statusScanner.js
@@ -44,8 +44,7 @@ casper.then(function() {
   var month = currentTime.getMonth() + 1;
   var day = currentTime.getDate();
   var year = currentTime.getFullYear();
-  //var path = './logs/results-'+year + '-' + month + '-' + day + '.html';
-  var path = './logs/index.html'
+  var path = './logs/results-'+year + '-' + month + '-' + day + '.html';
 
   this.each(links, function(self, link) {
     self.thenOpen((link), function(response) {
